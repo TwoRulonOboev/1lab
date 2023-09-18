209a17a859a6d9dd96aae80ecad04b611ff21612
Igor <Igor@DESKTOP-P25FOQ1>
2023-09-18 15:55:39 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+    //алгоритм Евклида для нахождения наибольшего общего делителя двух чисел
+    static int FindGCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    //определен массив  numbers
+    static void Main()
+    {
+        Random random = new Random();
+        int[] numbers = new int[5];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = random.Next(1, 100);
+        }
+
+        int gcd = 0;
+        Stopwatch stopwatch = new Stopwatch();
+
+        Thread thread1 = new Thread(() =>
+        {
+            stopwatch.Start();
+            gcd = FindGCD(numbers[0], numbers[1]);
+            Console.WriteLine("Greatest common divisor: " + gcd);
+            Console.WriteLine("Thread execution time: " + stopwatch.ElapsedMilliseconds + " mls");
+            stopwatch.Stop();
+        });
+
+        Thread thread2 = new Thread(() =>
+        {
+            stopwatch.Start();
+            gcd = FindGCD(numbers[2], numbers[3]);
+            Console.WriteLine("Greatest common divisor: " + gcd);
+            Console.WriteLine("Thread execution time: " + stopwatch.ElapsedMilliseconds + " mls");
+            stopwatch.Stop();
+        });
+
+        Thread thread3 = new Thread(() =>
+        {
+            stopwatch.Start();
+            gcd = FindGCD(numbers[3], numbers[4]);
+            Console.WriteLine("Greatest common divisor: " + gcd);
+            Console.WriteLine("Thread execution time: " + stopwatch.ElapsedMilliseconds + " mls");
+            stopwatch.Stop();
+        });
+
+        thread1.Start();
+        thread1.Join();
+        thread2.Start();
+        thread2.Join();
+        thread3.Start();
+        thread3.Join();
+    }
+}
